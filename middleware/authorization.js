54232d209f1cb139571ea96801f90afb71d0d3e1
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-06-08 23:20:34 +0530

    removed unnecessary console logs

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,9 +1,7 @@
 const { UnauthorizedError } = require('../errors');
 
 const authorize = async (req, res, next) => {
-  console.log(req.user);
   if (req.user.role === 'user') {
-    console.log('I am in this check');
     throw new UnauthorizedError('Not authorized to use this route');
   }
   if (req.user.role === 'admin') {
