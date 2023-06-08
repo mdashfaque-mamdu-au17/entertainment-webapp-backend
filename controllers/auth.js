b3831d40ab75040597cb8b7c376b5b5e2ad80c7e
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-06-08 23:13:30 +0530

    login and signup routes completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,44 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const {
+  BadRequestError,
+  UnauthenticatedError,
+} = require('../errors');
+
 const register = async (req, res) => {
-  res.end('Register user');
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      role: user.role,
+      token,
+    },
+  });
 };
 
 const login = async (req, res) => {
-  res.end('Login user');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Wrong password entered');
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      role: user.role,
+      token,
+    },
+  });
 };
 
-const updateUser = async (req, res) => {
-  res.end('update user');
-};
-
-module.exports = { register, login, updateUser };
+module.exports = { register, login };
